b25650ce816b49a98eb2fc1dda6d2496fb2bee1f
luojian <yw>
2021-02-17 23:56:59 +0800

    main layout

--- src/app/common/canvas/exCanvas.ts
+++ src/app/common/canvas/exCanvas.ts
@@ -123,38 +123,64 @@ class Rect {
         if(this['render']) {
             this['render'](ex);
         }
-        this.children.forEach(a => {
-            if(a['render']) {
-                a['render'](ex);
-            }
+        this.children.forEach(child => {
+            child.update(ex);
         })
         ex.context.restore();
     }
-    constructor(private _x = 0, private _y = 0, private _w = 0, private _h = 0, private _angle = 0, private parent: Rect = null) {
+    constructor(private _x = 0, private _y = 0, private _w = 0, private _h = 0, private _angle = 0, protected parent: Rect = null) {
     }
     translate(x, y){
         this._x = x;
         this._y = y;
     }
     scale(x, y){
-        this._w = x - this._x;
-        this._h = y - this._y;
+        this._w = x;
+        this._h = y;
     }
     rotate(x, y, angle = 0){
         if(!angle){
             let offsetX = x - this.x;
             let offsetY = y - this.y;
             this._angle = Math.atan(offsetY/offsetX);
+            console.log('rotate', x, y, this.x, this.y, offsetX, offsetY, this.angle)
         }else {
             this._angle = angle;
         }
         
     }
+    transform(x: number, y: number){
+        let angle = this.absolute('_angle');
+        let x1 = x - this.absolute('_x');
+        let y1 = y - this.absolute('_y');
+        if(angle > -0.00001 && angle < 0.00001){
+            return {x2: x1, y2: y1};
+        }
+        let cos = Math.cos(this.angle);
+        let sin = Math.sin(this.angle);
+        let x2 = x1 * cos - y1 * sin;
+        let y2 = x1 * sin + y1 * cos;
+        return {x2: x2, y2: y2};
+    }
+    hit(x1: number, y1: number){
+        let cos = Math.cos(this.angle);
+        let sin = Math.sin(this.angle);
+        let x2 = x1 * cos - y1 * sin;
+        let y2 = x1 * sin + y1 * cos;
+        return x2 > 0 && y2 > 0 && x2 < this.w && y2 < this.h;
+    }
     detect(x: number, y: number){
+        /* let X = this.absolute('_x');
+        let Y = this.absolute('_y');
+        let angle = this.absolute('_angle');
+        if(angle < -0.00001 || angle > 0.00001){
+            return this.hit(x - X, y - Y);
+        } 
+        return x > X && y > Y && x < X + this.w && y < Y + this.h;*/
         return x > this.x && y > this.y && x < this.x + this.w && y < this.y + this.h;
     }
-    relate(prop:string = '_x') {
-        let a = 0;
+    absolute(prop:string = '_x') {
+        let a = this[prop];
         let parent = this.parent;
         while(parent) {
             a += parent[prop];
@@ -166,10 +192,10 @@ class Rect {
         return this._angle;
     }
     get x(){
-        return this._x + this.relate('_x');
+        return this._x
     }
     get y(){
-        return this._y + this.relate('_y');
+        return this._y
     }
     get w(){
         return this._w;
@@ -188,7 +214,7 @@ class Circle extends Rect implements Renderable {
     render(ex: ExCanvasRenderingContext2D) {
         const radius = Math.min(this.w, this.h)/2.0;
         ex.context.beginPath();
-        ex.arc(this.x + radius, this.y + radius/2.0, radius, 0, 2 * Math.PI, false);
+        ex.arc(radius, 0, radius, 0, 2 * Math.PI, false);
         ex.context.fillStyle = this.color;
         ex.context.fill();
         ex.context.lineWidth = 1;
@@ -200,7 +226,7 @@ class Solid extends Rect implements Renderable {
     color = '#87ceeb';
     render(ex: ExCanvasRenderingContext2D) {
         ex.context.fillStyle = this.color;
-        ex.fillRect(this.x, this.y, this.w, this.h);
+        ex.fillRect(0, 0, this.w, this.h);
     }
 }
 class Border extends Rect implements Renderable {
@@ -210,15 +236,16 @@ class Border extends Rect implements Renderable {
         ex.context.lineWidth = this.width;
         ex.context.strokeStyle = this.color;
         ex.context.beginPath();
-        ex.rect(this.x + 0.5, this.y + 0.5, this.w, this.h);
+        ex.rect(0.5, 0.5, this.w, this.h);
         ex.stroke();
     }
 }
 class Group extends Rect implements Renderable {
-    pointRadius = 3;
+    pointRadius = 4;
     scalePoint: Solid = null;
     rotatePoint: Circle = null;
-    startPoint = {x: -1, y: -1};
+    startPoint: {x: number, y: number} = null;
+    endPoint = {x: -1, y: -1};
     border: Border = null;
     padding = 5;
     isFocus = false;
@@ -227,51 +254,61 @@ class Group extends Rect implements Renderable {
     
     mouseDown = false;
     constructor(protected ex: ExCanvasRenderingContext2D, x = 0, y = 0, w = 0, h = 0, ) {
-        super(x, y, w, h);
+        super();
         
-        this.scalePoint = new Solid(0, 0, this.pointRadius*2, this.pointRadius*2);
-        this.scalePoint.translate(w - this.pointRadius, h - this.pointRadius);
+        this.scalePoint = new Solid();
         this.attach(this.scalePoint);
 
-        this.rotatePoint = new Circle(0, 0, this.pointRadius*2, this.pointRadius*2);
-        this.rotatePoint.translate(w - this.pointRadius,  - this.pointRadius);
+        this.rotatePoint = new Circle();
         this.attach(this.rotatePoint);
 
-        this.border = new Border(0, 0, w, h);
-        this.border.translate(- this.padding, - this.padding);
-        this.border.scale(w, h);
+        this.border = new Border();
         this.attach(this.border);
+
+        this.onChange(x, y, w, h); 
+    }
+    onChange(x, y, w, h){
+        this.scale(w, h);
+        this.border.scale(this.w + (this.padding*2),  this.h + (this.padding*2));
+        this.rotatePoint.scale(this.pointRadius*2,  this.pointRadius*2);
+        this.scalePoint.scale(this.pointRadius*2, this.pointRadius*2);
+
+        this.border.translate(- this.padding, - this.padding);
+        this.rotatePoint.translate(this.w + this.padding - this.pointRadius, - this.pointRadius);
+        this.scalePoint.translate(this.w + this.padding - this.pointRadius, this.h + this.padding - this.pointRadius);
     }
     render(ex: ExCanvasRenderingContext2D) {
     }
     onMousedown(x: number, y: number) {
-        this.isFocus = this.detect(x, y);
-        this.isScale = this.scalePoint.detect(x, y);
-        this.isRotate = this.rotatePoint.detect(x, y);
-        if(this.isFocus) {
-            this.startPoint.x = x; 
-            this.startPoint.y = y;
+        let {x2, y2} = this.transform(x, y)
+        this.isFocus = this.detect(x2, y2);
+        this.isScale = this.scalePoint.detect(x2, y2);
+        this.isRotate = this.rotatePoint.detect(x2, y2);
+        if(!this.startPoint) {
+            this.startPoint = {x : super.absolute('_x'), y : super.absolute('_y')}
         }
-        console.log('onMousedown isFocus', this.isFocus, x, y, this.x, this.y, this.w, this.h);
+        if(this.isFocus){
+            this.endPoint.x = x - super.absolute('_x');
+            this.endPoint.y = y - super.absolute('_y');
+        }
+        console.log('onMousedown isRotate', this.isRotate);
     }
     onMousemove(x: number, y: number) {
-        console.log('isFocus', x, y, this.x, this.y, this.w, this.h)
         if(this.isFocus && !this.isScale && !this.isRotate){
-            this.translate(this.x + x - this.startPoint.x, this.y + y - this.startPoint.y);
-            this.startPoint.x = x; 
-            this.startPoint.y = y;
+            super.translate(x - this.startPoint.x - this.endPoint.x, y - this.startPoint.y - this.endPoint.y);       
         }else if(this.isScale) {
-            this.scale(x, y);
-            this.border.scale(x, y)
-            this.scalePoint.translate(x, y);
-            this.rotatePoint.translate(x, this.scalePoint.y);
+            this.onChange(x, y, x - this.startPoint.x, y - this.startPoint.y)
         }else if(this.isRotate) {
-            this.rotate(x, y);
+            super.rotate(x - this.startPoint.x, y - this.startPoint.y);
+
         }
+        console.log('onMousemove isRotate', this.isRotate, x, y, this.startPoint, x - this.startPoint.x, y - this.startPoint.y, this.absolute('_x'), this.absolute('_y'), super.angle);
     }
     onMouseup() {
+        this.isFocus = false;
         this.isScale = false;
         this.isRotate = false;
+        console.log('onMouseup',   this.x, this.y, this.w, this.h)
     }
 } 
 
@@ -280,7 +317,7 @@ export class Text extends Group {
     color = '#000';
     focusIndex = 0;
     constructor(ex: ExCanvasRenderingContext2D, public str = '') {
-        super(ex, 0, 0, 64, 32);
+        super(ex);
     }
     measureText(ex: ExCanvasRenderingContext2D, str: string) {
         let metrics = ex.context.measureText(str);
@@ -305,11 +342,7 @@ export class Text extends Group {
             W = x > W ? x : W;
             H = y;
         }
-        this.scale(W + offset.height + this.padding , H + this.padding);
-        //this.border.translate(- this.padding, - offset.height - this.padding);
-        this.border.scale(W + offset.height + this.padding , H + this.padding);
-        this.scalePoint.translate(W + offset.height + this.padding - this.pointRadius, H + this.padding - this.pointRadius );
-        this.rotatePoint.translate(W + offset.height + this.padding - this.pointRadius, - offset.height - this.padding);
+        this.onChange(- this.padding, - offset.height - this.padding, Math.max(this.w, W + offset.height + this.padding) , Math.max(this.h, H + offset.height + this.padding) );
     }
     render(ex: ExCanvasRenderingContext2D) {
         super.render(ex);
@@ -352,9 +385,17 @@ export class RenderManger extends Rect {
     }
     render() {
         this.ex.context.clearRect(0, 0, this.ex.canvas.width, this.ex.canvas.height);
+        this.ex.context.save();
+        this.ex.context.translate(this.x, this.y);
+        if(this.angle < -0.00001 || this.angle > 0.00001 ) {
+            this.ex.context.rotate(this.angle);
+        }
         this.children.forEach((child: Text) => {
             child.update(this.ex);
         })
+        
+        this.ex.context.restore();
+        
     }
     debug = 1;
     createText(){
@@ -369,16 +410,16 @@ export class RenderManger extends Rect {
         this.children.forEach((child: Text) => {
             console.log('onMousedown', event, this.x, this.y);
             child.onMousedown(
-                (event.x || event.changedTouches[0].clientX) - event.target.getBoundingClientRect().left - this.x, 
-                (event.y || event.changedTouches[0].clientY) - event.target.getBoundingClientRect().top - this.y);
+                (event.x || event.changedTouches[0].clientX) - event.target.getBoundingClientRect().left, 
+                (event.y || event.changedTouches[0].clientY) - event.target.getBoundingClientRect().top);
         })
         this.render();
     }
     onMousemove(event) {
         this.children.forEach((child: Text) => {
             child.onMousemove(
-                (event.x || event.changedTouches[0].clientX) - event.target.getBoundingClientRect().left - this.x, 
-                (event.y || event.changedTouches[0].clientY) - event.target.getBoundingClientRect().top - this.y);
+                (event.x || event.changedTouches[0].clientX) - event.target.getBoundingClientRect().left, 
+                (event.y || event.changedTouches[0].clientY) - event.target.getBoundingClientRect().top);
         })
         this.render();
     }
