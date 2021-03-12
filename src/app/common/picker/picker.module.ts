63e732705cf6e3ce323c2cfc1b3981c22b310091
luojian@example.com <luojian@example.com>
2021-03-12 14:48:24 +0800

    main layout color

--- src/app/common/picker/picker.module.ts
+++ src/app/common/picker/picker.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { ColorPicker } from './CPNT.picker';
 
 @NgModule({
     imports: [
         CommonModule,
+        FlexLayoutModule
     ],
     declarations: [
         ColorPicker,
