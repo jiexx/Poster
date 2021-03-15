a1bf6d12caa369f7b8224968925119b2874c2f33
luojian@example.com <luojian@example.com>
2021-03-15 17:02:30 +0800

    main layout color

--- src/app/common/picker/CPNT.picker.ts
+++ src/app/common/picker/CPNT.picker.ts
@@ -13,8 +13,12 @@ export class ColorPicker {
     constructor() {
         for(let i = 1 ; i < 11 ; i ++) {
             for(let j = 1 ; j < 11 ; j ++) {
-                this.colors.push(this.hslToHex(j*36, 1, i*15))
+                console.log(Math.pow(i,0.7)*36)
+                this.colors.push(this.hslToHex(j*36, 1, Math.pow(i,0.7)*36 ))
             }
+        }
+        for(let i = 0 ; i < 10 ; i ++) {
+            this.colors.push(this.hslToHex(0, 0, i*35))
         }
     }
     hslToHex(h, s, l) {
