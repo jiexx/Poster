81fe20fd6212e1880bdec5fa3102912436fc3308
luojian <yw>
2021-03-15 18:44:40 +0800

    main layout color

--- src/app/common/picker/CPNT.picker.ts
+++ src/app/common/picker/CPNT.picker.ts
@@ -11,16 +11,34 @@ export class ColorPicker {
     
     colors = [];
     constructor() {
-        for(let i = 1 ; i < 11 ; i ++) {
-            for(let j = 1 ; j < 11 ; j ++) {
-                this.colors.push(this.hslToHex(j*36, 1, i*15))
+        for(let i = 0 ; i < 10 ; i ++) {
+            for(let j = 0 ; j < 10 ; j ++) {
+                this.colors.push(this.hslToRgb(i*36, j*0.1, 1))
             }
         }
     }
-    hslToHex(h, s, l) {
-        let a=s*Math.min(l,1-l);
-        let f= (n,k=(n+h/30)%12) => l - a*Math.max(Math.min(k-3,9-k,1),-1);                 
-        return `rgb(${f(0)}, ${f(8)}, ${f(4)})`;
+    hue2rgb(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    }
+    hslToRgb(h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = this.hue2rgb(p, q, h + 1/3);
+            g = this.hue2rgb(p, q, h);
+            b = this.hue2rgb(p, q, h - 1/3);
+        }
+        console.log(h,s,l, r,g,b)
+        return `rgb(${Math.round(r * 255)}, ${Math.round(g * 255)}, ${Math.round(b * 255)})`;
     }
 
 }
